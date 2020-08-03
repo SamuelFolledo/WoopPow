cc9e952bf338bb029e46bc5bdea60bd6a64b19ba
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-03 03:09:58 -0400

    [add] reference to background

--- iOS/WoopPow/WoopPow/Helpers/Constants.swift
+++ iOS/WoopPow/WoopPow/Helpers/Constants.swift
@@ -10,11 +10,19 @@ import UIKit
 
 struct Constants {
     struct Images {
-        let punchUpLight: UIImage = UIImage(named: "punchUp")!
-        let punchUpMedium: UIImage = UIImage(named: "punchUp")!
-        let punchUpHard: UIImage = UIImage(named: "punchUpHard")!
-        let punchDownLight: UIImage = UIImage(named: "punchDown")!
-        let punchDownMedium: UIImage = UIImage(named: "punchDown")!
-        let punchDownHard: UIImage = UIImage(named: "punchDownHard")!
+        //Backgrounds
+        static let gameControllerBackground1: UIImage = UIImage(named: "gameControllerBackground1")!
+        //Moves
+        static let moveUp: UIImage = UIImage(named: "moveUp")!
+        static let moveBack: UIImage = UIImage(named: "moveBack")!
+        static let moveDown: UIImage = UIImage(named: "moveDown")!
+        static let moveForward: UIImage = UIImage(named: "moveForward")!
+        //Punches
+        static let punchUpLight: UIImage = UIImage(named: "punchUp")!
+        static let punchUpMedium: UIImage = UIImage(named: "punchUp")!
+        static let punchUpHard: UIImage = UIImage(named: "punchUpHard")!
+        static let punchDownLight: UIImage = UIImage(named: "punchDown")!
+        static let punchDownMedium: UIImage = UIImage(named: "punchDown")!
+        static let punchDownHard: UIImage = UIImage(named: "punchDownHard")!
     }
 }
