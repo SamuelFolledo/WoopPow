892ad83e0f81659cd0f173c5374c3b636b14ff88
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-03 01:29:38 -0400

    [add] screen needed for game in its own directories

---
+++ iOS/WoopPow/WoopPow/Game/Game/GameController.swift
@@ -0,0 +1,72 @@
+//
+//  GameController.swift
+//  WoopPow
+//
+//  Created by Samuel Folledo on 8/3/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class GameController: UIViewController {
+    
+    //MARK: Properties
+    var coordinator: MainCoordinator!
+    
+    //MARK: Views
+    private let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "gameVCBackground1")!
+        return imageView
+    }()
+    
+    //MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    //MARK: Private Methods
+    fileprivate func setupViews() {
+        setupNavigationBar()
+        setupBackground()
+        
+        let moveSetView = MoveSetView(isLeft: true)
+        moveSetView.containerView.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        view.addSubview(moveSetView)
+        moveSetView.snp.makeConstraints { (make) in
+            make.left.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.centerY.equalTo(view.safeAreaLayoutGuide)
+            make.width.equalTo(view.safeAreaLayoutGuide.snp.height).multipliedBy(0.4)
+            make.height.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.6)
+        }
+        
+        let moveSetView2 = MoveSetView(isLeft: false)
+        moveSetView2.containerView.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        view.addSubview(moveSetView2)
+        moveSetView2.snp.makeConstraints { (make) in
+            make.right.equalTo(view.safeAreaLayoutGuide).offset(-10)
+            make.centerY.equalTo(view.safeAreaLayoutGuide)
+            make.width.equalTo(view.safeAreaLayoutGuide.snp.height).multipliedBy(0.4)
+            make.height.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.6)
+        }
+    }
+    
+    fileprivate func setupNavigationBar() {
+//        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    fileprivate func setupBackground() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(backgroundImageView)
+        backgroundImageView.snp.makeConstraints { (make) in
+            make.top.left.right.bottom.equalTo(view)
+        }
+    }
+    
+    //MARK: Helpers
+}
+
+//MARK: Extensions
---
+++ iOS/WoopPow/WoopPow/Game/GameOver/GameOverController.swift
@@ -0,0 +1,32 @@
+//
+//  GameOverController.swift
+//  WoopPow
+//
+//  Created by Samuel Folledo on 8/3/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class GameOverController: UIViewController {
+    
+    //MARK: Properties
+    
+    //MARK: Views
+    
+    //MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    //MARK: Private Methods
+    fileprivate func setupViews() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    //MARK: Helpers
+}
+
+//MARK: Extensions
+
