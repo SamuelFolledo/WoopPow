62f9ab3986e4966b206e9442b24314b9b658021b
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-03 06:37:03 -0400

    [add] hp

--- iOS/WoopPow/WoopPow/Models/Game.swift
+++ iOS/WoopPow/WoopPow/Models/Game.swift
@@ -9,19 +9,25 @@
 import UIKit
 
 struct Game {
+    //MARK: Constants
+    let initialHp: Int = 100
     
+    //MARK: Properties
     var player1: Player
     var player2: Player
-    var player1HP: Int = 30
-    var player2HP: Int = 30
+    var player1Hp: Int
+    var player2Hp: Int
     
     var gameId: String = ""
     var text: String?
     var winnerUid: String?
     var roundNumber = 0
+    var gameType: String?
     
     init(player1: Player, player2: Player) {
         self.player1 = player1
         self.player2 = player2
+        self.player1Hp = initialHp
+        self.player2Hp = initialHp
     }
 }
