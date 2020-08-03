0e3b18590bffe79d61bf259dbf80b9cd336daece
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-03 03:58:50 -0400

    [add] player

--- iOS/WoopPow/WoopPow/Models/Player.swift
+++ iOS/WoopPow/WoopPow/Models/Player.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct Player {
+    
+    var name: String
+    var playerId: String
+    
+    //implement later
+    var imageUrl: String?
+    var image: UIImage?
+    var userType: String?
+    var server: String = "0.1.0"
+    
+    //MARK: Initializers
+    init(name: String, playerId: String) {
+        self.name = name
+        self.playerId = playerId
+    }
+}
