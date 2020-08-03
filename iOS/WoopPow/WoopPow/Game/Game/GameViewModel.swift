38b00011b8da287cd9a73a918ad270b456d64f09
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-03 06:36:41 -0400

    [add] put most functions that needs the game here

--- iOS/WoopPow/WoopPow/Game/Game/GameViewModel.swift
+++ iOS/WoopPow/WoopPow/Game/Game/GameViewModel.swift
@@ -8,11 +8,42 @@
 
 import Foundation
 
-struct GameViewModel {
+class GameViewModel {
     
+    //MARK: Properties
     let game: Game
+    weak var delegate: GameController?
+    let player1HPProgress = Progress(totalUnitCount: 30)
+    let player2HPProgress = Progress(totalUnitCount: 30)
+    var timeLeftTimer: Timer?
+    var timeLeftCounter: Int = 8 {
+        didSet { delegate?.timeLeftLabel.text = "\(timeLeftCounter)" }
+    }
     
+    //MARK: Init
     init(game: Game) {
         self.game = game
+    }
+    
+    deinit {
+        timeLeftTimer?.invalidate()
+    }
+    
+    //MARK: Methods
+    
+    func startTimeLeftTimer() {
+        timeLeftTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTurnTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTurnTime() {
+        timeLeftCounter -= 1
+        if timeLeftCounter == 0 {
+            timeLeftTimer?.invalidate()
+            //            setupSelectedTag()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.timeLeftCounter = 8
+                self.startTimeLeftTimer()
+            }
+        }
     }
 }
