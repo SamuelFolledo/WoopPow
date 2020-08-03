d10daca07e0589b0a8ff17f6cb338edf7f1eeb2a
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-03 03:11:07 -0400

    [add] time left timer and label

--- iOS/WoopPow/WoopPow/Game/Game/GameController.swift
+++ iOS/WoopPow/WoopPow/Game/Game/GameController.swift
@@ -12,27 +12,86 @@ class GameController: UIViewController {
     
     //MARK: Properties
     var coordinator: MainCoordinator!
+    var timeLeftTimer: Timer?
+    var timeLeftCounter: Int = 8 {
+        didSet { timeLeftLabel.text = "\(timeLeftCounter)" }
+    }
+    let player1HPProgress = Progress(totalUnitCount: 30)
+    let player2HPProgress = Progress(totalUnitCount: 30)
     
     //MARK: Views
-    private let backgroundImageView: UIImageView = {
+    private lazy var backgroundImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.clipsToBounds = false
-        imageView.contentMode = .scaleAspectFill
-        imageView.image = UIImage(named: "gameVCBackground1")!
+        imageView.contentMode = .scaleToFill
+        imageView.image = Constants.Images.gameControllerBackground1
         return imageView
+    }()
+    
+    private lazy var timeLeftLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .font(size: 32, weight: .bold, design: .default)
+        label.text = "\(timeLeftCounter)"
+        label.numberOfLines = 1
+        return label
     }()
     
     //MARK: App Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        startTurnTimer()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        timeLeftTimer?.invalidate()
     }
     
     //MARK: Private Methods
-    fileprivate func setupViews() {
-        setupNavigationBar()
+    
+    private func startTurnTimer() {
+        timeLeftTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTurnTime), userInfo: nil, repeats: true)
+    }
+    
+    //MARK: Helpers
+    @objc func updateTurnTime() {
+        timeLeftCounter -= 1
+        if timeLeftCounter == 0 {
+            timeLeftTimer?.invalidate()
+//            setupSelectedTag()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.timeLeftCounter = 8
+                self.startTurnTimer()
+            }
+        }
+    }
+}
+
+//MARK: Extensions
+
+//MARK: Setup and Constraints Views
+private extension GameController {
+    func setupViews() {
+        self.navigationController?.navigationBar.isHidden = true
         setupBackground()
-        
+        constraintTopViews()
+        constraintMoveSets()
+    }
+    
+    func constraintTopViews() {
+        view.addSubview(timeLeftLabel)
+        timeLeftLabel.snp.makeConstraints {
+            $0.height.lessThanOrEqualTo(view.safeAreaLayoutGuide.snp.height).multipliedBy(0.1)
+            $0.width.lessThanOrEqualTo(view.safeAreaLayoutGuide.snp.height).multipliedBy(0.15)
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+        }
+    }
+    
+    func constraintMoveSets() {
         let moveSetView = MoveSetView(isLeft: true)
         moveSetView.containerView.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         view.addSubview(moveSetView)
@@ -54,19 +113,11 @@ class GameController: UIViewController {
         }
     }
     
-    fileprivate func setupNavigationBar() {
-//        self.navigationController?.navigationBar.isHidden = true
-    }
-    
-    fileprivate func setupBackground() {
+    func setupBackground() {
         view.backgroundColor = .systemBackground
         view.addSubview(backgroundImageView)
         backgroundImageView.snp.makeConstraints { (make) in
             make.top.left.right.bottom.equalTo(view)
         }
     }
-    
-    //MARK: Helpers
 }
-
-//MARK: Extensions
