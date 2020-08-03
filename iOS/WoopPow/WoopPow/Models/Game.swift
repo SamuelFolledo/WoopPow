b255bad25da3f768142d91daa25495a735f38b46
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-03 03:58:55 -0400

    [add] game model

--- iOS/WoopPow/WoopPow/Models/Game.swift
+++ iOS/WoopPow/WoopPow/Models/Game.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct Game {
+    
+    var player1: Player
+    var player2: Player
+    var player1HP: Int = 30
+    var player2HP: Int = 30
+    
+    var gameId: String = ""
+    var text: String?
+    var winnerUid: String?
+    var roundNumber = 0
+    
+    init(player1: Player, player2: Player) {
+        self.player1 = player1
+        self.player2 = player2
+    }
+}
