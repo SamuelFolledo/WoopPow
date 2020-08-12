cf3006ee8d7b94bd4c96733d5977c90c39b83bfe
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-11 20:14:42 -0400

    [add] direction and position

---
+++ iOS/WoopPow/WoopPow/Models/Direction.swift
@@ -0,0 +1,17 @@
+//
+//  Direction.swift
+//  WoopPow
+//
+//  Created by Samuel Folledo on 8/10/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
+
+enum Direction {
+    case up, down, mid
+}
+
+enum AttackPosition {
+    case upLight, upMedium, upHard, downLight, downMedium, downHard
+}
