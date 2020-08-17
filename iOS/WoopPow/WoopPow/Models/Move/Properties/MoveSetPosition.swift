f1185a54b8743f8df0d4b8be9fafb14fb43d636d
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-17 04:01:10 -0400

    [add] different moveSet position

---
+++ iOS/WoopPow/WoopPow/Models/Move/Properties/MoveSetPosition.swift
@@ -0,0 +1,13 @@
+//
+//  MoveSetPosition.swift
+//  WoopPow
+//
+//  Created by Samuel Folledo on 8/17/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
+
+enum MoveSetPosition {
+    case up, back, down, forward, none
+}
