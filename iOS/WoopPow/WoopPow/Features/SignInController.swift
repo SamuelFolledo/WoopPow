fe679f7a07946ca06e43288b3cc7984ee62cd0f4
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-17 20:57:35 -0400

    [add] sign in page

---
+++ iOS/WoopPow/WoopPow/Features/SignInController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInController.swift
+//  WoopPow
+//
+//  Created by Samuel Folledo on 8/17/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class SignInController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
