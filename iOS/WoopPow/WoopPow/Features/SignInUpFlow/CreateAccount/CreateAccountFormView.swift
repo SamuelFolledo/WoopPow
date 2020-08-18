df354caf623e94372e542b1341f45c9c753d195f
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-17 21:57:39 -0400

    [add] forms and underliendTextField

---
+++ iOS/WoopPow/WoopPow/Features/SignInUpFlow/CreateAccount/CreateAccountFormView.swift
@@ -0,0 +1,9 @@
+//
+//  CreateAccountFormView.swift
+//  WoopPow
+//
+//  Created by Samuel Folledo on 8/17/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/WoopPow/WoopPow/Views/UnderlinedTextField.swift
@@ -0,0 +1,9 @@
+//
+//  UnderlinedTextField.swift
+//  WoopPow
+//
+//  Created by Samuel Folledo on 8/17/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
