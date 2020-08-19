bd7e0e952ec3e12189a8d248c41585e5c551d353
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-19 01:49:40 -0400

    [add] a way to make navbar clear

--- iOS/WoopPow/WoopPow/Helpers/Extensions/UIViewController+Extensions.swift
+++ iOS/WoopPow/WoopPow/Helpers/Extensions/UIViewController+Extensions.swift
@@ -36,6 +36,20 @@ extension UIViewController {
         Constants.Views.indicatorView.removeFromSuperview()
     }
     
+    ///Transparent Navigation Bar
+    func transparentNavigationBar() {
+        // Make the navigation bar background clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.backgroundColor = .clear
+    }
+    
+    func removeTransparentNavigationBar() {
+        navigationController?.navigationBar.setBackgroundImage(nil, for: .default)
+        navigationController?.navigationBar.shadowImage = nil
+    }
+    
     ///remove keyboard when view is tapped
     func hideKeyboardOnTap() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
