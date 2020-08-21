b5a67a85990601687995557908a7b0920d6f4594
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-21 01:44:29 -0400

    [add] add imageView content mode and background on buttons

--- iOS/WoopPow/WoopPow/Services/AppService.swift
+++ iOS/WoopPow/WoopPow/Services/AppService.swift
@@ -12,12 +12,16 @@ struct AppService {
     static func playButton() -> UIButton {
         let button = UIButton(frame: .zero)
         button.setImage(Constants.Images.playButton, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.backgroundColor = .clear
         return button
     }
     
     static func backButton() -> UIButton {
         let button = UIButton(frame: .zero)
         button.setImage(Constants.Images.backButton, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.backgroundColor = .clear
         return button
     }
     
@@ -25,30 +29,40 @@ struct AppService {
         let button = UIButton(frame: .zero)
         button.setImage(Constants.Images.homeButton, for: .normal)
         button.setImage(Constants.Images.homeButtonSelected, for: .selected)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.backgroundColor = .clear
         return button
     }
     
     static func noButton() -> UIButton {
         let button = UIButton(frame: .zero)
         button.setImage(Constants.Images.noButton, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.backgroundColor = .clear
         return button
     }
     
     static func yesButton() -> UIButton {
         let button = UIButton(frame: .zero)
         button.setImage(Constants.Images.yesButton, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.backgroundColor = .clear
         return button
     }
     
     static func restartButton() -> UIButton {
         let button = UIButton(frame: .zero)
         button.setImage(Constants.Images.restartButton, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.backgroundColor = .clear
         return button
     }
     
     static func resumeButton() -> UIButton {
         let button = UIButton(frame: .zero)
         button.setImage(Constants.Images.resumeButton, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.backgroundColor = .clear
         return button
     }
 }
