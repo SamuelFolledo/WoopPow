7f11b5c463beb99bdda31b146672e8fb4e7ac1c4
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-21 02:41:29 -0400

    [add] setup views for the navBar

--- iOS/WoopPow/WoopPow/Views/NavBarView.swift
+++ iOS/WoopPow/WoopPow/Views/NavBarView.swift
@@ -1,13 +1,58 @@
 //
-//  HomeTopView.swift
+//  NavBarView.swift
 //  WoopPow
 //
 //  Created by Samuel Folledo on 8/21/20.
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
-import UIKit
+import SnapKit
 
-class HomeTopView {
+class NavBarView: UIView {
     
+    //MARK: Properties
+    var player: Player
+    
+    //MARK: Views
+    lazy var containerView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 15
+        view.layer.masksToBounds = false
+        view.backgroundColor = .clear
+        view.layer.shadowOffset = CGSize(width: 0, height: 4)
+        view.layer.shadowOpacity = 0.4
+        view.layer.shadowRadius = 3
+        view.layer.shadowColor = UIColor.gray.cgColor
+        return view
+    }()
+    private lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = Constants.Images.navBarView
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    //MARK: Init
+    required init(player: Player) {
+        self.player = player
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: Private Methods
+    
+    fileprivate func setupView() {
+        addSubview(backgroundImageView)
+        backgroundImageView.snp.makeConstraints { (make) in
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+        addSubview(containerView)
+        containerView.snp.makeConstraints { (make) in
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
 }
