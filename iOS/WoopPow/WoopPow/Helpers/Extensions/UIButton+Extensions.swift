73cfbd564bae08c0076c7ea879fba6366520ceaa
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-21 00:51:40 -0400

    [add] removed button initialization

--- iOS/WoopPow/WoopPow/Helpers/Extensions/UIButton+Extensions.swift
+++ iOS/WoopPow/WoopPow/Helpers/Extensions/UIButton+Extensions.swift
@@ -10,40 +10,4 @@ import UIKit.UIButton
 
 extension UIButton {
     
-    func asBackButton() -> UIButton {
-        let button = UIButton(frame: .zero)
-        button.setImage(Constants.Images.backButton, for: .normal)
-        return button
-    }
-    
-    func asHomeButton() -> UIButton {
-        let button = UIButton(frame: .zero)
-        button.setImage(Constants.Images.homeButton, for: .normal)
-        button.setImage(Constants.Images.homeButtonSelected, for: .selected)
-        return button
-    }
-    
-    func asNoButton() -> UIButton {
-        let button = UIButton(frame: .zero)
-        button.setImage(Constants.Images.noButton, for: .normal)
-        return button
-    }
-    
-    func asYesButton() -> UIButton {
-        let button = UIButton(frame: .zero)
-        button.setImage(Constants.Images.yesButton, for: .normal)
-        return button
-    }
-    
-    func asRestartButton() -> UIButton {
-        let button = UIButton(frame: .zero)
-        button.setImage(Constants.Images.restartButton, for: .normal)
-        return button
-    }
-    
-    func isResumeButton() -> UIButton {
-        let button = UIButton(frame: .zero)
-        button.setImage(Constants.Images.resumeButton, for: .normal)
-        return button
-    }
 }
