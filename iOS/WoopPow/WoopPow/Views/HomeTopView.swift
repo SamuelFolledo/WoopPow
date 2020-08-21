3a26ad7197c497181f71238b0dba1d08ba636976
Samuel Folledo <samuelfolledo@gmail.com>
2020-08-21 00:55:07 -0400

    [add] top view for home

---
+++ iOS/WoopPow/WoopPow/Views/HomeTopView.swift
@@ -0,0 +1,13 @@
+//
+//  HomeTopView.swift
+//  WoopPow
+//
+//  Created by Samuel Folledo on 8/21/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class HomeTopView {
+    
+}
