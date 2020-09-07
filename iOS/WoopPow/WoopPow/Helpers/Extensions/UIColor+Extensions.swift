0f0a441868598a19e04357fa24ed70c0152c3aac
Samuel Folledo <samuelfolledo@gmail.com>
2020-09-07 18:30:58 -0400

    [add] new color

--- iOS/WoopPow/WoopPow/Helpers/Extensions/UIColor+Extensions.swift
+++ iOS/WoopPow/WoopPow/Helpers/Extensions/UIColor+Extensions.swift
@@ -11,6 +11,7 @@ import UIKit
 extension UIColor {
     
     static let woopPowYellow = UIColor(r: 250, g: 227, b: 15, a: 1)
+    static let woopPowLightBlue = UIColor(r: 0, g: 255, b: 255, a: 1)
     
     convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
         self.init(red: r/255, green: g/255, blue: b/255, alpha: a)
