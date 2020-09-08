3deb31d2b07af2d647ec204eba332c6c6850af72
Samuel Folledo <samuelfolledo@gmail.com>
2020-09-08 03:05:24 -0400

    [add] a way to add gradient on labels

--- iOS/WoopPow/WoopPow/Helpers/Extensions/UIImage+Extensions.swift
+++ iOS/WoopPow/WoopPow/Helpers/Extensions/UIImage+Extensions.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
-import Foundation
+import UIKit.UIImage
+
+extension UIImage {
+    
+    /// add a gradient color to texts
+    /// - Usage: let gradientImage = UIImage.gradientImageWithBounds(bounds: myLabel.bounds, colors: [firstColor.cgColor, secondColor.cgColor])
+    /// - Application: myLabel.textColor = UIColor.init(patternImage: gradientImage)
+    /// - Source: https://stackoverflow.com/questions/1266179/how-do-i-add-a-gradient-to-the-text-of-a-uilabel-but-not-the-background
+    static func gradientImageWithBounds(bounds: CGRect, colors: [CGColor]) -> UIImage {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
