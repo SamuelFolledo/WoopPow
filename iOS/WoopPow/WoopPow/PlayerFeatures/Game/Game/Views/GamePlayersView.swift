6be1f2e2cfbcf2c07598c2ac637b73f2aecab937
Samuel Folledo <samuelfolledo@gmail.com>
2020-10-17 07:55:25 -0400

    [add] new assets

---
+++ iOS/WoopPow/WoopPow/PlayerFeatures/Game/Game/Views/GamePlayersView.swift
@@ -0,0 +1,21 @@
+//
+//  GamePlayersView.swift
+//  WoopPow
+//
+//  Created by Samuel Folledo on 10/17/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class GamePlayersView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
